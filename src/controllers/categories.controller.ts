2e0bde2ff054c2a8dc4c67ddade485676102e018
Leonardo Fuentes Claros <leonardofuentesclaros@gmail.com>
2025-05-29 02:05:13 -0400

    Add: getOne and update categories endpoints

--- src/controllers/categories.controller.ts
+++ src/controllers/categories.controller.ts
@@ -1,18 +1,6 @@
 import { Request, Response, NextFunction } from 'express';
 import pool from '../db';
 
-export const getAllCategories = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  try {
-    const allCategories = await pool.query('SELECT * FROM categories');
-    res.json(allCategories.rows);
-  } catch (error) {
-    next(error);
-  }
-};
 
 export const createCategorie = async (
   req: Request,
@@ -32,10 +20,69 @@ export const createCategorie = async (
   }
 };
 
+
+export const getAllCategories = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const allCategories = await pool.query('SELECT * FROM categories ORDER BY name ASC');
+    res.json(allCategories.rows);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getOneCategorie = async (req: Request, res: Response, next: NextFunction) => {
+  const { id } = req.params;
+  try {
+    const categorie = await pool.query('SELECT name, description FROM categories WHERE id=$1', [id])
+    res.json(categorie.rows[0]);
+  } catch (error) {
+    next(error);
+  }
+}
+
+
+export const updateCategorie = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+  const client = await pool.connect();
+
+  try {
+    const { id } = req.params;
+    const { name, description } = req.body;
+
+    //Begin transaction
+    await client.query('BEGIN');
+
+    // Update categorie
+    const result = await client.query('UPDATE categories SET name=$1, description=$2 WHERE id=$3 RETURNING *', [name, description, id]);
+
+    // There's no category
+    if (result.rowCount === 0) {
+      await client.query('ROLLBACK');
+      res.status(404).json({ message: 'Category not found' });
+      return;
+    }
+
+    // Commit in case all works good
+    await client.query('COMMIT');
+    res.status(200).json({
+      message: "Categorie updated successufully",
+      category: result.rows[0]
+    });
+  } catch (error) {
+    // Rollback in case something wrong happend
+    await client.query("ROLLBACK");
+    next(error);
+  } finally {
+    client.release(); // Leave connection
+  }
+}
+
 export const deleteCategorie = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
   try {
     const id = parseInt(req.params.id, 10);
-    // Suponiendo pool.query está definido y retorna Promise<{rowCount: number, rows: any[]}>
     const result = await pool.query('DELETE FROM categories WHERE id=$1 RETURNING *', [id]);
     if (result.rowCount === 0) {
       res.status(404).json({ message: "Categorie not found" });
@@ -46,5 +93,6 @@ export const deleteCategorie = async (req: Request, res: Response, next: NextFun
     next(error);
   }
 }
+
 
 
--- src/routes/categoriesRoutes.ts
+++ src/routes/categoriesRoutes.ts
@@ -1,11 +1,13 @@
 import { Router } from 'express';
-import { createCategorie, deleteCategorie, getAllCategories } from '../controllers/categories.controller';
+import { createCategorie, deleteCategorie, getAllCategories, getOneCategorie, updateCategorie } from '../controllers/categories.controller';
 
 const router = Router();
 console.log(typeof deleteCategorie)
 router.get('/categories', getAllCategories);
+router.get('/categories/:id', getOneCategorie);
 router.post('/categories', createCategorie);
 router.delete('/categories/:id', deleteCategorie);
+router.put('/categories/:id', updateCategorie);
 
 
 export default router;
